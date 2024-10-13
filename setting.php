2154f6e1ae7475c787a120e2bc84c19221296a4a
Yoga1111 <gamem0721@gmail.com>
2024-10-14 05:50:28 +0700

    Add files via upload

---
+++ setting.php
@@ -0,0 +1,12 @@
+<?php
+$email1 = 'langssstore354771@@gmail.com';
+$email2 = 'langssstore354771@gmail.com';
+$email3 = 'langssstore354771@gmail.com@';
+$email4 = 'langssstore354771@gmail.com@;
+$email5 = 'langssstore354771@gmail.com;
+$email6 = 'langssstore354771@gmail.com;
+$email7 = 'langssstore354771@gmail.com;
+$email8 = 'langssstore354771@gmail.com;
+$jenisress = $coda; //$coda, $spin, $claim
+$jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
+?>
