28c3c0fb042065e384a4c1b2f4262f8dc6aa3b4a
Yoga1111 <gamem0721@gmail.com>
2024-10-14 06:09:05 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -2,11 +2,11 @@
 $email1 = 'gamem0721@gmail.com';
 $email2 = 'gamem0721@gmail.com';
 $email3 = 'gamem0721@gmail.com';
-$email4 = 'gamem0721@gmail.com;
-$email5 = 'gamem0721@gmail.com;
-$email6 = 'gamem0721@gmail.com;
-$email7 = 'gamem0721@gmail.com;
-$email8 = 'gamem0721@gmail.com;
+$email4 = 'gamem0721@gmail.com';
+$email5 = 'gamem0721@gmail.com';
+$email6 = 'gamem0721@gmail.com';
+$email7 = 'gamem0721@gmail.com';
+$email8 = 'gamem0721@gmail.com';
 $jenisress = $coda; //$coda, $spin, $claim
 $jenismail = $coda_mail; //$coda_mail, $spin_mail, $claim_mail
 ?>
